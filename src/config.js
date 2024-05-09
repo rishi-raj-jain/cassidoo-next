7245353fe6611ae3afa288433dab25026b52fae6
Cassidy Williams <cassidoo@users.noreply.github.com>
2024-05-09 15:43:52 -0500

    Update header link highlighting

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "Cassidy Williams";
+export const SITE_TAGLINE = "Software Engineer in Chicago";
 export const SITE_DESCRIPTION =
-	"I'm Cassidy Williams, a software engineer, mom, and overall nerd in Chicago. I write about tech, projects, and whatever else I'm thinking about!";
+  "I'm Cassidy Williams, a software engineer, mom, and overall nerd in Chicago. I write about tech, projects, and whatever else I'm thinking about!";
